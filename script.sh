86589b15e74cfe7d573abbc0b308b107e218466f
edgar <edgarglzcruz@outlook.com>
2019-01-31 08:23:06 -0600

    Program finished

--- script.sh
+++ script.sh
@@ -3,14 +3,14 @@
 fname="fdownloaded.csv"
 
 clear
-wget -q -O output/"$fname" http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv
+wget -q -O "$fname" http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv
 #We change the windows end of file format to linux
 sed -i -e "s/\r/\n/g" output/"$fname"
 echo -e "\nNumber of rows:"
-wc -l < output/"$fname"
+wc -l < "$fname"
 echo -e "\nPrinting first 10 rows:"
-head output/"$fname"
+head "$fname"
 echo -e "\nPrinting last 10 rows:"
-tail output/"$fname"
+tail "$fname"
 echo -e "\nLooking for Multi-Family:"
-cat output/"$fname" | grep -e Multi-Family
+cat "$fname" | grep -e Multi-Family
--- src/main/java/com/edgartest/app/App.java
+++ src/main/java/com/edgartest/app/App.java
@@ -1,13 +1,59 @@
 package com.edgartest.app;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.*;
+//import org.junit.*;
+import org.apache.log4j.Logger;
+
 public class App 
 {
+    //private static Logger logger = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        long startTime = System.currentTimeMillis();
+        if(args.length != 1){
+            //logger.error("Incorrect number of arguments, provide just one argument with the name of the csv file");
+        }
+        else{
+            String csvFile = args[0];
+            if (!csvFile.matches("(.)*[.]csv")) {
+                //logger.error("Incorrect filename or filetype");
+            }
+            else{
+                //logger.debug("File name: " + csvFile);
+
+                String line = "";
+                String delimiter = ",";
+                int mfCount = 0;
+
+                try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+                    System.out.println("\nLines with Multi-Family token:");
+                    while ((line = br.readLine()) != null) {
+                        String[] fields = line.split(delimiter);
+                        // I wanted to be explicit, it could be code in just one foreach loop though
+                        for (String field : fields) {
+                            String[] tokens = field.split(" ");
+                            for (String token : tokens) {
+                                if (token.contains("Multi-Family")) {
+                                    mfCount++;
+                                    System.out.println(line);
+                                }
+                            }
+                        }
+                    }
+                    System.out.println("\nWordcount:" + mfCount);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        long endTime   = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("\nRunning Time (Seconds): " + totalTime/1000.0);
     }
 }
